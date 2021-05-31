94075af6a45ad7342725d5b4910853b5bbd9f559
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2021-05-31 12:53:10 +0200

    script javascript simplifié

--- Zagoujava/script.js
+++ Zagoujava/script.js
@@ -1,13 +1,6 @@
 const fantom = document.querySelector(".fantom");
 const bascule = document.querySelector(".bascule");
 
-bascule.addEventListener("mousedown", () => {
-  fantom.style.display = "none"
-
-});
-bascule.addEventListener("mouseup", () => {
-  fantom.style.display="block"
-});
-bascule.addEventListener("touchstart", () => {
-  fantom.style.display = "none";
+bascule.addEventListener("click", () => {
+  fantom.style.display = fantom.style.display === "none" ? "block" : "none";
 });
