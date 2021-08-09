d9d46e12fa0e5ab3c7ee3cc8769458715143594e
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2021-08-10 01:39:36 +0200

    imZoom finalisée commentée

--- Zagoujava/imZoom.js
+++ Zagoujava/imZoom.js
@@ -1,3 +1,4 @@
+/* ------------------------------------------------------- */
 /* dimension des images enfants du DIV */
 let dimZoom = (el) => {
   const h = el.children[0].naturalHeight;
@@ -11,6 +12,7 @@ let dimZoom = (el) => {
     el.children[0].style.height = 95 + "vh";
   }
 };
+/* ------------------------------------------------------- */
 /* crée une div de classe .zoom et l'insere sur le body */
 let zoomImage = (image) => {
   const div = document.createElement("div");
@@ -23,15 +25,7 @@ let zoomImage = (image) => {
   dimZoom(div); /* calculer les dimensions du zoom */
   document.body.appendChild(div); /* afficher le Zoom */
 };
-/* cherche la div class="zoom" et la ferme, si elle existe */
-let ferme = (e) => {
-  e.preventDefault();
-  const div = document.querySelector(".zoom");
-
-  div.parentNode.removeChild(div);
-  document.querySelector(".blind").classList.replace("blind", "image");
- 
-};
+/* -------------------------------------------------------- */
 /* Tableau des images img */
 const tab = Array.from(document.querySelectorAll(".image img"));
 /* tableau des outerHTML des images */
@@ -39,45 +33,57 @@ const outers = [];
 tab.forEach((el) => {
   outers.push(el.outerHTML);
 });
-
+/* ---------------------------------------------------------- */
 /* si on pointe sur le container "image", on zoome, et si on releve le pointer on arrete le zoom */
-
 const stockImages = document.querySelector(".image");
 stockImages.addEventListener("click", (e) => {
   e.preventDefault();
-  
+  /* ==============fait disparaitre la Div "image et la remplace par "blind */
   if (document.querySelector(".image") !== null) {
     document.querySelector(".image").classList.replace("image", "blind");
   } else {
+    /* si on a cliqué sur une image (hors de zoom), stop et attente d'un click sur zoom ou une key pour fermer ou prec et suivant pour autres images*/
     return;
   }
- 
-  /* index de l'image cliquée */
+  /* ==============index de l'image cliquée */
   let numero = outers.indexOf(e.target.outerHTML);
-
   /* fonction changer de slide +1 suiv -1 prec */
   let ChangeSlide = (sens) => {
     numero = numero + sens;
-    if (numero < 0) numero = outers.length - 1;
-    if (numero > outers.length - 1) numero = 0;
-    document.querySelector(".zoom img").outerHTML = outers[numero];
+    if (numero < 0) {
+      numero = outers.length - 1;
+    }
+    if (numero > outers.length - 1) {
+      numero = 0;
+    }
+    document.querySelector(".zoom img").outerHTML = outers[numero];/* remplacer l'image par celle de numero +/-1 */
     dimZoom(zoom); /* retailler l'image */
   };
-  zoomImage(e.target.outerHTML); /* zoomer l'image cliquée */
+  /* ==============zoomer l'image cliquée */
+  zoomImage(e.target.outerHTML);
+
   const prec = document.getElementById("precedent");
   const suiv = document.getElementById("suivant");
   const zoom = document.querySelector(".zoom");
-  /* click fleche Gauche => Précédent */
+
+  /* ==============ferme la div Zoom et remet la classe à image */
+  let ferme = (e) => {
+    e.preventDefault();
+    zoom.parentNode.removeChild(zoom);
+    document.querySelector(".blind").classList.replace("blind", "image");
+  };
+  /* ==============click fleche Gauche => Précédent */
   prec.addEventListener("click", (e) => {
     ChangeSlide(-1);
+    /* l'event reste sur la div prec, et ne remonte pas dans "blind" */
     e.stopPropagation();
   });
-  /* click fleche Gauche => Suivant */
+  /* ==============click fleche droite => Suivant */
   suiv.addEventListener("click", (e) => {
     ChangeSlide(1);
     e.stopPropagation();
   });
-  /* fleche droite =>Prec, fleche gauche=>suiv */
+  /* ==============fleche droite =>Prec, fleche gauche=>suiv */
   document.addEventListener("keydown", (e) => {
     if (e.key === "ArrowLeft") {
       ChangeSlide(-1);
@@ -86,6 +92,7 @@ stockImages.addEventListener("click", (e) => {
       ChangeSlide(1);
     }
   });
+  /* ============== fermer en cliquant sur Zoomo une touche*/
   zoom.addEventListener("click", ferme);
   document.addEventListener("keypress", ferme);
 });
