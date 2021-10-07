b019b0fad268e70e99b730114b16974ccbad5c65
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2021-10-07 19:59:27 +0200

    menu hamburger smartphone réduit et progressif

--- Zagoujava/scriptYT.js
+++ Zagoujava/scriptYT.js
@@ -3,7 +3,10 @@
 const hamburg = document.querySelector(".hamb");
 const menuH = document.querySelector(".menu ul");
 hamburg.addEventListener("click", () => {
-  menuH.style.display = menuH.style.display === "block" ? "none" : "block";
+  menuH.style.transition = "opacity 0.5s  linear";
+  menuH.style.opacity = menuH.style.opacity === "1" ? "0" : "1";
+  menuH.style.visibility =
+    menuH.style.visibility === "visible" ? "hidden" : "visible";
 });
 
 /* bascule Iframe YT vers image de fond */
