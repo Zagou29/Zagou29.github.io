d08f3e2b00ef55e1bbe541215621ab6665301813
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2022-02-03 18:54:36 +0100

    rajouter le module fullScreen.js

--- Zagoujava/imZoom.js
+++ Zagoujava/imZoom.js
@@ -1,4 +1,5 @@
 import { menuGlissant } from "./menuYT.js";
+import { go_fullScreen, stop_fullScreen } from "./fullScreen.js";
 /* -------------------------------------------------------- */
 const fix_men = document.querySelector(".fixmenu");
 const entete = document.querySelector(".entete");
@@ -71,35 +72,6 @@ const av_ar = () => {
 };
 
 /* ------------------------------------------ */
-const go_fullScreen = (elem) => {
-  if (elem) {
-    let not_fs =
-      !document.fullscreenElement &&
-      !document.mozFullScreen &&
-      !document.webkitIsFullScreen &&
-      !document.msFullscreenElement;
-    if (not_fs) {
-      if (elem.requestFullscreen) elem.requestFullscreen();
-      else if (elem.mozRequestFullScreen) elem.mozRequestFullScreen();
-      else if (elem.webkitRequestFullscreen) elem.webkitRequestFullscreen();
-      else if (elem.msRequestFullscreen) elem.msRequestFullscreen();
-    } else stop_fullScreen();
-  }
-};
-
-const stop_fullScreen = () => {
-  let fs =
-    document.fullscreenElement ||
-    document.mozFullScreen ||
-    document.webkitIsFullScreen ||
-    document.msFullscreenElement;
-  if (fs) {
-    if (document.exitFullscreen) document.exitFullscreen();
-    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
-    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
-    else if (document.msExitFullscreen) document.msExitFullscreen();
-  }
-};
 
 const fulls = () => {
   document.addEventListener("keydown", (e) => {
